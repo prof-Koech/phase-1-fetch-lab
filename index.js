0bc0772ee61e27ac7ad59281c886a28bc7eea2f0
Kennedy <kiplangatkennedy019@gmail.com>
2023-03-31 12:32:56 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
   
 }
 
@@ -15,3 +18,25 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+// //Ensure that you return fech(URL)
+// // on line 6 we pass a function
+// function fetchBooks() {
+//   return fetch("https://anapioficeandfire.com/api/books")
+//     .then(resp => resp.json())
+//     .then(json => renderBooks(json))
+// }
+
+// function renderBooks(books) {
+//   const main = document.querySelector('main')
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2')
+//     h2.innerHTML = book.name
+//     main.appendChild(h2)
+//   })
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks()
+// })
